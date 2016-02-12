e964668885cc1f9742f5854a678824c992c4e63d
Natasha Murashev <nmurashev@gmail.com>
2016-02-12 11:25:37 +0900

    made minor change on opening twitter urls

--- trySwift/SpeakerDetailViewController.swift
+++ trySwift/SpeakerDetailViewController.swift
@@ -64,13 +64,13 @@ extension SpeakerDetailViewController: TwitterFollowDelegate {
     func followUser(username: String) {
         let twitterURLs = [
             "twitter://user?screen_name=\(username)", // Twitter
-            "tweetbot:///user_profile/\(username)", // TweetBot
-            "echofon:///user_timeline?\(username)", // Echofon
-            "twit:///user?screen_name=\(username)", // Twittelator Pro
+            "tweetbot://user_profile/\(username)", // TweetBot
+            "echofon://user_timeline?\(username)", // Echofon
+            "twit://user?screen_name=\(username)", // Twittelator Pro
             "x-seesmic://twitter_profile?twitter_screen_name=\(username)", // Seesmic
             "x-birdfeed://user?screen_name=\(username)", // Birdfeed
-            "tweetings:///user?screen_name=\(username)", // Tweetings
-            "simplytweet:?link=http://twitter.com/\(username)", // SimplyTweet
+            "tweetings://user?screen_name=\(username)", // Tweetings
+            "simplytweet://?link=http://twitter.com/\(username)", // SimplyTweet
             "icebird://user?screen_name=\(username)", // IceBird
             "fluttr://user/\(username)", // Fluttr
         ]
@@ -81,6 +81,7 @@ extension SpeakerDetailViewController: TwitterFollowDelegate {
             if let url = NSURL(string: twitterURL) where application.canOpenURL(url) && !applicationOpened {
                 application.openURL(url)
                 applicationOpened = true
+                break
             }
         }
 
