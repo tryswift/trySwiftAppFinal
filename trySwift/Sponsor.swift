502fd39653d0f6c0e8119ab3b14cc246a761dc82
Natasha Murashev <nmurashev@gmail.com>
2016-02-16 07:02:31 +0900

    added github sponsor description

--- trySwift/Sponsor.swift
+++ trySwift/Sponsor.swift
@@ -173,7 +173,7 @@ extension Sponsor {
         twitter: "github",
         logo: "github",
         level: .Silver,
-        description: nil
+        description: isJapanese ? nil : "GitHub is the best way to build software together. Whether it's your company's app, your favorite open source library, or a weekend side project, GitHub helps everyone work together better by providing tools for easier collaboration and code sharing. Catch up on what happened while you were out or ask for help on a tricky chunk of code. Manage and contribute to projects from all your devices. The best part is you can keep using all of your favorite tools. Start collaborating on code today—open source project hosting is free!"
     )
     
     static let mixi = Sponsor(
