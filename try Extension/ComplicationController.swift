74ed331275b77babf85f9852e26719e74b9888bc
Natasha Murashev <nmurashev@gmail.com>
2016-02-21 17:34:57 +0900

    added watch os extension

---
+++ try Extension/ComplicationController.swift
@@ -0,0 +1,71 @@
+//
+//  ComplicationController.swift
+//  try Extension
+//
+//  Created by Natasha Murashev on 2/21/16.
+//  Copyright © 2016 NatashaTheRobot. All rights reserved.
+//
+
+import ClockKit
+
+
+class ComplicationController: NSObject, CLKComplicationDataSource {
+    
+}
+
+// MARK: - Timeline Configuration
+extension ComplicationController {
+    
+    func getSupportedTimeTravelDirectionsForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTimeTravelDirections) -> Void) {
+        handler([.Forward, .Backward])
+    }
+    
+    func getTimelineStartDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
+        handler(nil)
+    }
+    
+    func getTimelineEndDateForComplication(complication: CLKComplication, withHandler handler: (NSDate?) -> Void) {
+        handler(nil)
+    }
+    
+    func getPrivacyBehaviorForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationPrivacyBehavior) -> Void) {
+        handler(.ShowOnLockScreen)
+    }
+}
+
+// MARK: - Timeline Population
+extension ComplicationController {
+    
+    func getCurrentTimelineEntryForComplication(complication: CLKComplication, withHandler handler: ((CLKComplicationTimelineEntry?) -> Void)) {
+        // Call the handler with the current timeline entry
+        handler(nil)
+    }
+    
+    func getTimelineEntriesForComplication(complication: CLKComplication, beforeDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
+        // Call the handler with the timeline entries prior to the given date
+        handler(nil)
+    }
+    
+    func getTimelineEntriesForComplication(complication: CLKComplication, afterDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
+        // Call the handler with the timeline entries after to the given date
+        handler(nil)
+    }
+}
+
+// MARK: - Update Scheduling
+extension ComplicationController {
+    
+    func getNextRequestedUpdateDateWithHandler(handler: (NSDate?) -> Void) {
+        // Call the handler with the date when you would next like to be given the opportunity to update your complication content
+        handler(nil)
+    }
+}
+
+// MARK: - Placeholder Templates
+extension ComplicationController {
+    
+    func getPlaceholderTemplateForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTemplate?) -> Void) {
+        // This method will be called once per supported complication, and the results will be cached
+        handler(nil)
+    }
+}
---
+++ try Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  try Extension
+//
+//  Created by Natasha Murashev on 2/21/16.
+//  Copyright © 2016 NatashaTheRobot. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
