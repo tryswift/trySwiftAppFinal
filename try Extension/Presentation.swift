3bb5a748470fc1adf2e21c49fce26eb5519412e0
Natasha Murashev <nmurashev@gmail.com>
2016-02-23 09:28:35 +0900

    initial configuration for complication

---
+++ try Extension/Presentation.swift
@@ -0,0 +1,13 @@
+//
+//  Presentation.swift
+//  trySwift
+//
+//  Created by Natasha Murashev on 2/21/16.
+//  Copyright © 2016 NatashaTheRobot. All rights reserved.
+//
+
+import UIKit
+
+class Presentation: NSObject {
+
+}
---
+++ try Extension/Speaker.swift
@@ -0,0 +1,13 @@
+//
+//  Speaker.swift
+//  trySwift
+//
+//  Created by Natasha Murashev on 2/21/16.
+//  Copyright © 2016 NatashaTheRobot. All rights reserved.
+//
+
+import UIKit
+
+class Speaker: NSObject {
+
+}
