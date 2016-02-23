c8fc11cb8e27b665781f4a5fd565cdcbc8ef5f40
Natasha Murashev <nmurashev@gmail.com>
2016-02-23 15:40:43 +0900

    added glance

--- try Extension/GlanceController.swift
+++ try Extension/GlanceController.swift
@@ -7,20 +7,36 @@
 //
 
 import WatchKit
-import Foundation
-
+import Timepiece
 
 class GlanceController: WKInterfaceController {
 
+    @IBOutlet private var titleInterfaceLabel: WKInterfaceLabel!
+    @IBOutlet private var timeInterfaceLabel: WKInterfaceLabel!
+    @IBOutlet private var speakerInterfaceImage: WKInterfaceImage!
+    @IBOutlet private var nameInterfaceLabel: WKInterfaceLabel!
+    @IBOutlet private var twitterInterfaceLabel: WKInterfaceLabel!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        configureDefault()
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        let sessions = Session.sessions
+        
+        let sessionForTime = sessions.filter { (($0.startTime - 5.minutes)...$0.endTime).contains(NSDate())}
+        if let session = sessionForTime.first {
+            configureSession(session)
+        } else if NSDate() < Session.sessions.first!.startTime {
+            configureSession(Session.sessions.first!)
+        } else {
+            configureDefault()
+        }
     }
 
     override func didDeactivate() {
@@ -28,4 +44,31 @@ class GlanceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+}
+
+private extension GlanceController {
+    
+    func configureDefault() {
+        titleInterfaceLabel.setText("try! Conference Tokyo, 🇯🇵")
+        timeInterfaceLabel.setText("Mar 2 - Mar 4")
+        speakerInterfaceImage.setImage(UIImage(named: "tryLogo"))
+        nameInterfaceLabel.setText("try! 🤗")
+        twitterInterfaceLabel.setText("@tryswiftconf")
+    }
+    
+    func configureSession(session: Session) {
+        if let speaker = session.speaker {
+            titleInterfaceLabel.setText(speaker.presentation.title)
+            speakerInterfaceImage.setImage(UIImage(named: speaker.image))
+            nameInterfaceLabel.setText(speaker.name)
+            twitterInterfaceLabel.setText("@\(speaker.twitter)")
+        } else {
+            titleInterfaceLabel.setText(session.description)
+            speakerInterfaceImage.setImage(UIImage(named: "tryLogo"))
+            nameInterfaceLabel.setText("try! 🤗")
+            twitterInterfaceLabel.setText("@tryswiftconf")
+        }
+        
+        timeInterfaceLabel.setText(session.timeString)
+    }
 }
