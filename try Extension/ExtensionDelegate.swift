748c114b280ca6225d79a216dc6c196dbfcada70
Natasha Murashev <nmurashev@gmail.com>
2016-02-23 11:01:31 +0900

    complication now working

--- try Extension/ExtensionDelegate.swift
+++ try Extension/ExtensionDelegate.swift
@@ -11,7 +11,7 @@ import WatchKit
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidFinishLaunching() {
-        // Perform any final initialization of your application.
+        NSTimeZone.setDefaultTimeZone(NSTimeZone(abbreviation: "JST")!)
     }
 
     func applicationDidBecomeActive() {
