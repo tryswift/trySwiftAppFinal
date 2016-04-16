fe502a7ad2d5c93460284dce7e7c70b9292eff1a
Bas Broek <bas@basbroek.nl>
2016-04-16 20:33:33 +0200

    Added organizers

--- trySwift/Organizer.swift
+++ trySwift/Organizer.swift
@@ -32,56 +32,15 @@ extension Organizer: JSONDecodable {
 
 extension Organizer {
 
-    static let organizers: [Organizer] = []// [natasha, kk, caesar, tasanobu, helen, hector, vaish, minwoo]
-
-//    static let natasha = Organizer(
-//        name: isJapanese ? "ナターシャ ザ ロボット" : "Natasha Murashev",
-//        twitter: "NatashaTheRobot",
-//        image: Toucan(image: UIImage(named: "natasha")!).maskWithEllipse().image
-//    )
-//
-//    static let kk = Organizer(
-//        name: isJapanese ? "岸川克己" : "Kishikawa Katsumi",
-//        twitter: "k_katsumi",
-//        image: Toucan(image: UIImage(named: "kk")!).maskWithEllipse().image
-//    )
-//
-//    static let vaish = Organizer(
-//        name: "Vaishnavi Srinivasan",
-//        twitter: "vaish",
-//        image: Toucan(image: UIImage(named: "vaish")!).maskWithEllipse().image
-//    )
-//
-//    static let helen = Organizer(
-//        name: "Helen Holmes",
-//        twitter: "helenvholmes",
-//        image: Toucan(image: UIImage(named: "hellenholmes")!).maskWithEllipse().image
-//    )
-//
-//    static let caesar = Organizer(
-//        name: isJapanese ? "Caesar Wirth" : "Caesar Wirth",
-//        twitter: "cjwirth",
-//        image: Toucan(image: UIImage(named: "caesar")!).maskWithEllipse().image
-//    )
-//
-//    static let minwoo = Organizer(
-//        name: "Minwoo Park",
-//        twitter: "tebica",
-//        image: Toucan(image: UIImage(named: "minwoo")!).maskWithEllipse().image
-//    )
-//
-//    static let hector = Organizer(
-//        name: "Hector Matos",
-//        twitter: "allonsykraken",
-//        image: Toucan(image: UIImage(named: "hector")!).maskWithEllipse().image
-//    )
-//
-//    static let tasanobu = Organizer(
-//        name: isJapanese ? "田坂和暢" : "Kazunobu Tasaka",
-//        twitter: "tasanobu",
-//        image: Toucan(image: UIImage(named: "tasanobu")!).maskWithEllipse().image
-//    )
-//
+    static let organizers: [Organizer] = {
+        do {
+            return try dataJSON().array("speakers").filter { try $0.bool("organizer") }.map(Organizer.init)
+        } catch {
+            print(error)
+            return []
+        }
+    }()
+    
     static let trySwift = Organizer(
         name: "try! Conference",
         twitter: "tryswiftconf",
