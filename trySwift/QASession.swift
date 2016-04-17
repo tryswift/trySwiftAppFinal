205ef90268a3326ec757e10324f597a16d6df864
Bas Broek <bas@basbroek.nl>
2016-04-17 10:20:53 +0200

    Fixed dateFormats, filter sessions by day

--- trySwift/QASession.swift
+++ trySwift/QASession.swift
@@ -35,7 +35,7 @@ struct QASession {
 
 extension QASession: JSONDecodable {
     init(json: JSON) throws {
-        let jsonDateFormat = "yyyy-mm-dd'T'hh:mm:ss"
+        let jsonDateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         self.id = try json.int("id")
         self.startTime = try json.string("startTime").dateFromFormat(jsonDateFormat)!
         self.endTime = try json.string("endTime").dateFromFormat(jsonDateFormat)!
@@ -54,4 +54,11 @@ extension QASession {
             return []
         }
     }()
+}
+
+extension QASession: DayFilterable {
+    
+    static func qaSessions(forDate date: NSDate) -> [QASession] {
+        return qaSessions.filter { $0.sameDay(asDate: date) }
+    }
 }
--- trySwift/Session.swift
+++ trySwift/Session.swift
@@ -31,7 +31,7 @@ struct Session {
 extension Session: JSONDecodable {
     
     init(json: JSON) throws {
-        let jsonDateFormat = "yyyy-mm-dd'T'hh:mm:ss"
+        let jsonDateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         self.id = try json.int("id")
         self.startTime = try json.string("startTime").dateFromFormat(jsonDateFormat)!
         self.endTime = try json.string("endTime").dateFromFormat(jsonDateFormat)!
@@ -52,4 +52,11 @@ extension Session {
             return []
         }
     }()
+}
+
+extension Session: DayFilterable {
+
+    static func sessions(forDate date: NSDate) -> [Session] {
+        return sessions.filter { $0.sameDay(asDate: date) }
+    }
 }
