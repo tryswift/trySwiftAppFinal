0eb61c93ac0a641afee569e5582885fb393bbfdd
Bas Broek <bas@basbroek.nl>
2016-07-23 18:36:47 +0200

    Show events on August 31

---
+++ trySwift/SessionDataSourceDay3.swift
@@ -0,0 +1,14 @@
+//
+//  SessionDataSourceDay3.swift
+//  trySwift
+//
+//  Created by Bas Broek on 7/23/16.
+//  Copyright © 2016 NatashaTheRobot. All rights reserved.
+//
+
+import Foundation
+
+struct SessionDataSourceDay3: SessionDataSourceProtocol {
+    var header = "Fri, Sep 2"
+    var sessions = Session.sessions(forDate: NSDate.date(year: 2016, month: 09, day: 02))
+}
