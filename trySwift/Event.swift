08eca344ccbe09cba8ece7314de5135d47630115
NatashaTheRobot <natasha@natashatherobot.com>
2016-08-16 10:19:43 -0400

    completed first two days schedule

--- trySwift/Event.swift
+++ trySwift/Event.swift
@@ -13,3 +13,20 @@ struct Event {
     let location: String
     let website: NSURL
 }
+
+extension Event {
+    
+    static let gaWorkshop = Event(
+        title: "FREE Swift Workshop",
+        sponsor: "General Assembly",
+        logo: "ga_trimmed",
+        location: "10 East 21st Street, New York, NY",
+        website: NSURL(string: "https://generalassemb.ly/education/try-swift-workshop/new-york-city/27431")!)
+    
+    static let meetup = Event(
+        title: "Pre-conference Meetup",
+        sponsor: "Meetup",
+        logo: "meetup-square",
+        location: "632 Broadway, New York, NY",
+        website: NSURL(string: "http://www.meetup.com/Tech-Talks-at-Meetup/events/233150456/")!)
+}
