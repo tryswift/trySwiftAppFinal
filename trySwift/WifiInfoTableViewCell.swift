49fa4bbcb7663d151fe08119f77c7ce663743bbf
NatashaTheRobot <natasha@natashatherobot.com>
2016-08-21 08:53:03 -0400

    added wifi info to venue

---
+++ trySwift/WifiInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WifiInfoTableViewCell.swift
+//  trySwift
+//
+//  Created by Natasha Murashev on 8/21/16.
+//  Copyright © 2016 NatashaTheRobot. All rights reserved.
+//
+
+import UIKit
+
+class WifiInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
