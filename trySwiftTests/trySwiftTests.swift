2672077a62628d24402cd03dce8ffbc2de2d12c6
NatashaTheRobot <natasha@natashatherobot.com>
2016-09-20 10:10:11 +0200

    updated to Swift 3

--- trySwiftTests/trySwiftTests.swift
+++ trySwiftTests/trySwiftTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import trySwift
 
 class trySwiftTests: XCTestCase {
     
@@ -28,7 +27,7 @@ class trySwiftTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
