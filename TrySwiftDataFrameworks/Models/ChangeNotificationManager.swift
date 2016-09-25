5bd6896a441a732648e012067fac3c160e80f371
NatashaTheRobot <natasha@natashatherobot.com>
2016-09-25 10:34:31 +0200

    moved realm notification subscription logic to framework layer

---
+++ TrySwiftDataFrameworks/Models/ChangeNotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  ChangeNotificationManager.swift
+//  TrySwiftData
+//
+//  Created by Natasha Murashev on 9/25/16.
+//  Copyright © 2016 Natasha Murashev. All rights reserved.
+//
+
+import UIKit
+
+class ChangeNotificationManager: NSObject {
+
+}
