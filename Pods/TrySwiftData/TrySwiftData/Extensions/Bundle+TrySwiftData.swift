a3e778d5a22483db4310683952f936779fae41e2
Tim Oliver <me@timoliver.com.au>
2017-02-26 23:04:50 -0800

    Removed bundle copy logic

--- Pods/TrySwiftData/TrySwiftData/Extensions/Bundle+TrySwiftData.swift
+++ Pods/TrySwiftData/TrySwiftData/Extensions/Bundle+TrySwiftData.swift
@@ -8,46 +8,12 @@
 
 import Foundation
 
-var sharedGroupName: String?
-
 extension Bundle {
 
-    public class func setTrySwiftSharedGroupIdentifier(_ name: String) {
-        sharedGroupName = name
-    }
-
     public class func trySwiftDataBundle() -> Bundle {
-        // Return the shared groups bundle
-//        let sharedBundleURL = Bundle.trySwiftSharedBundleURL()
-//        if sharedBundleURL != nil && FileManager.default.fileExists(atPath: sharedBundleURL!.path) {
-//            if let bundle = Bundle(url: sharedBundleURL!) {
-//                return bundle
-//            }
-//        }
-
         // Return the Resource Bundle
         let bundleURL = Bundle.trySwiftResourceBundleURL()
         return Bundle(url: bundleURL)!
-    }
-
-    public class func copyTrySwiftBundleToSharedGroup() -> Bool {
-        let sharedBundleURL = Bundle.trySwiftSharedBundleURL()
-        let resourceBundleURL = Bundle.trySwiftResourceBundleURL()
-
-        guard sharedBundleURL != nil else {
-            return false
-        }
-
-        guard !FileManager.default.fileExists(atPath: sharedBundleURL!.path) else {
-            return true
-        }
-
-        do {
-            try FileManager.default.copyItem(at: resourceBundleURL, to: sharedBundleURL!)
-        }
-        catch { return false }
-
-        return true
     }
 
     public class func trySwiftResourceBundleURL() -> URL {
@@ -56,19 +22,6 @@ extension Bundle {
             return bundleURL
         }
         return Bundle.main.bundleURL
-    }
-
-    public class func trySwiftSharedBundleURL() -> URL? {
-        guard sharedGroupName != nil else {
-            return nil
-        }
-
-        guard let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: sharedGroupName!) else {
-            return nil
-        }
-
-        let bundleURL = containerURL.appendingPathComponent("TrySwiftData.bundle")
-        return bundleURL
     }
 
     public class func trySwiftAssetURL(for name: String) -> URL? {
