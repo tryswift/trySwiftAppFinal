6ec5ef9b9cb91bd5422d3b79d2d5cbdd1fe2ddcc
Tim Oliver <me@timoliver.com.au>
2017-02-26 19:53:42 -0800

    Reorganized layout of widget

--- Pods/Target Support Files/Pods-try! Today/Pods-try! Today-umbrella.h
+++ Pods/Target Support Files/Pods-try! Today/Pods-try! Today-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_try__TokyoVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_try__TokyoVersionString[];
+FOUNDATION_EXPORT double Pods_try__TodayVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_try__TodayVersionString[];
 
