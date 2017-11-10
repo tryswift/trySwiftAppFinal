a8f221417aaba00fed66c875405e7b163e8f7087
NatashaTheRobot <natasha@natashatherobot.com>
2017-11-10 16:39:21 +0700

    added icons for watch, fixed warning

--- trySwift/UITableViewExtension.swift
+++ trySwift/UITableViewExtension.swift
@@ -8,13 +8,13 @@
 
 extension UITableView {
     
-    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+    func register<T: UITableViewCell>(_: T.Type) {
         
         let nib = UINib(nibName: T.nibName, bundle: nil)
         register(nib, forCellReuseIdentifier: T.reuseIdentifier)
     }
     
-    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
