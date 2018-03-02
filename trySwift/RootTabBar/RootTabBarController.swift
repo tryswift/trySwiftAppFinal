d7656c81d7264706b14ff2bbf5010d86d3d7d2e8
Sash Zats <sash@zats.io>
2018-03-02 12:31:26 +0900

    Also scroll to the top if already at the first view controller

--- trySwift/RootTabBar/RootTabBarController.swift
+++ trySwift/RootTabBar/RootTabBarController.swift
@@ -16,10 +16,36 @@ class RootTabBarController: UITabBarController, UITabBarControllerDelegate {
 
   func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
     if let splitViewController = viewController as? UISplitViewController {
-      if let navigationController = splitViewController.viewControllers.last as? UINavigationController,
-        navigationController.viewControllers.count > 1 {
-        navigationController.popViewController(animated: true)
+      if let navigationController = splitViewController.viewControllers.last as? UINavigationController {
+        if navigationController.viewControllers.count > 1 {
+          navigationController.popViewController(animated: true)
+        } else {
+          navigationController.viewControllers.first?.scrollToTop()
+        }
       }
     }
+  }
+}
+
+private extension UIViewController {
+  func scrollToTop() {
+    let scrollView = view.findScrollSubview()
+    scrollView?.setContentOffset(.zero, animated: true)
+  }
+}
+
+private extension UIView {
+  func findScrollSubview() -> UIScrollView? {
+    if let scrollView = self as? UIScrollView,
+      scrollView.contentSize.width < scrollView.contentSize.height {
+      return scrollView
+    }
+    for subview in self.subviews {
+      if let scrollView = subview.findScrollSubview(),
+        scrollView.contentSize.width < scrollView.contentSize.height {
+        return scrollView
+      }
+    }
+    return nil
   }
 }
